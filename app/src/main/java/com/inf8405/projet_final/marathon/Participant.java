e1bdae9dbb8cda05ce2463d7e61e8a3d8c521556
Youssef ZEMMAHI <youssef.zemmahi@polymtl.ca>
2016-04-04 20:07:48 -0400

    ajout de la classe temperature, position, marathon et fonction d'acces et changement

--- app/src/main/java/com/inf8405/projet_final/marathon/Participant.java
+++ app/src/main/java/com/inf8405/projet_final/marathon/Participant.java
@@ -7,8 +7,16 @@ import java.util.UUID;
  */
 public class Participant {
 
-    String id;
+    private String id;
+    private String nom_=new String();
+    private String courriel_=new String();
+    private String password_=new String();
     private UUID uidFormat_ = UUID.fromString("91c83b36-e25c-11e5-9730-9a79f06e9478");
+    private Position position_=new Position();
+    private double distanceParcourue_=0.0;
+    private double vitesse_=0.0;
+    private double vitesseMoyenne_=0.0;
+    private int classement_=0;
 
     public Participant()
     {
@@ -21,5 +29,68 @@ public class Participant {
 
     public void setId(String id) {
         this.id = id;
+    }
+    public Position getPosition() {
+        return position_;
+    }
+
+    public void setPosition(Position position) {
+        this.position_ = position;
+    }
+
+    public double getDistanceParcourue() {
+        return distanceParcourue_;
+    }
+
+    public void setDistanceParcourue(double distanceParcourue) {
+        this.distanceParcourue_ = distanceParcourue;
+    }
+
+    public double getVitesse() {
+        return vitesse_;
+    }
+
+    public void setVitesse(double vitesse) {
+        this.vitesse_ = vitesse;
+    }
+
+    public double getVitesseMoyenne() {
+        return vitesseMoyenne_;
+    }
+
+    public void setVitesseMoyenne(double vitesseMoyenne) {
+        this.vitesseMoyenne_ = vitesseMoyenne;
+    }
+
+    public String getNom() {
+        return nom_;
+    }
+
+    public void setNom(String nom) {
+        this.nom_ = nom;
+    }
+
+    public String getCourriel() {
+        return courriel_;
+    }
+
+    public void setCourriel(String courriel) {
+        this.courriel_ = courriel;
+    }
+
+    public String getPassword() {
+        return password_;
+    }
+
+    public void setPassword(String password) {
+        this.password_ = password;
+    }
+
+    public int getClassement() {
+        return classement_;
+    }
+
+    public void setClassement(int classement) {
+        this.classement_ = classement;
     }
 }
